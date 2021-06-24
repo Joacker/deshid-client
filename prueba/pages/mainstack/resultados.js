be7c885ac7cc6545e73b93f42b08371cba21553f
ColaDuMacaco <joaquin.fernandez1@mail.udp.cl>
2021-06-23 23:51:12 -0400

    Front Update

--- prueba/pages/mainstack/resultados.js
+++ prueba/pages/mainstack/resultados.js
@@ -2,7 +2,7 @@
 import { View, Text, StyleSheet, Button, StatusBar, TextInput,
     SafeAreaView,
     Dimensions,
-    ScrollView, } from 'react-native';
+    ScrollView,TouchableHighlight } from 'react-native';
 import React, { Component } from 'react';
 import {
     LineChart,
@@ -12,90 +12,98 @@ import {
     ContributionGraph,
     StackedBarChart,
   } from 'react-native-chart-kit';
-
+import ProgressCircle from 'react-native-progress-circle'
+import { render } from 'react-native-web';
 // create a component
 const Resultados = () => {
-    /*return (
-        <>
-          <Text style={styles.header}>Progress Chart</Text>
-          <ProgressChart
-            data={[0.4, 0.6, 0.8]}
-            width={Dimensions.get('window').width - 16}
-            height={220}
-            chartConfig={{
-              backgroundColor: '#1cc910',
-              backgroundGradientFrom: '#eff3ff',
-              backgroundGradientTo: '#efefef',
-              decimalPlaces: 2,
-              color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-              style: {
-                borderRadius: 16,
-              },
-            }}
-            style={{
-              marginVertical: 8,
-              borderRadius: 16,
-            }}
-          />
-        </>
-      );*/
-      return (
-        <>
-          <Text style={styles.header}>Pie Chart</Text>
-          <PieChart
-            data={[
-              {
-                name: 'Seoul',
-                population: 21500000,
-                color: 'rgba(131, 167, 234, 1)',
-                legendFontColor: '#7F7F7F',
-                legendFontSize: 15,
-              },
-              {
-                name: 'Toronto',
-                population: 2800000,
-                color: '#F00',
-                legendFontColor: '#7F7F7F',
-                legendFontSize: 15,
-              },
-              {
-                name: 'New York',
-                population: 8538000,
-                color: '#ffffff',
-                legendFontColor: '#7F7F7F',
-                legendFontSize: 15,
-              },
-              {
-                name: 'Moscow',
-                population: 11920000,
-                color: 'rgb(0, 0, 255)',
-                legendFontColor: '#7F7F7F',
-                legendFontSize: 15,
-              },
-            ]}
-            width={Dimensions.get('window').width - 16}
-            height={220}
-            chartConfig={{
-              backgroundColor: '#1cc910',
-              backgroundGradientFrom: '#eff3ff',
-              backgroundGradientTo: '#efefef',
-              decimalPlaces: 2,
-              color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-              style: {
-                borderRadius: 16,
-              },
-            }}
-            style={{
-              marginVertical: 8,
-              borderRadius: 16,
-            }}
-            accessor="population"
-            backgroundColor="transparent"
-            paddingLeft="15"
-            absolute //for the absolute number remove if you want percentage
-          />
-        </>
-      );
+  return(
+    <View style={{alignItems:'center',flex:1,marginTop:100,marginBottom:200,flexDirection:'column'}}>
+    <View style={{justifyContent:'center',flex:1,flexDirection:'row',margin:10}}>
+    <View style={{
+      width: 140,
+      height: 140,
+      borderRadius: 140 / 2,
+      backgroundColor: '#0a9396',
+      justifyContent:'center',
+      margin:10,
+      alignSelf:'center'
+    }}>
+      <Text style={{
+        alignSelf: 'center',
+        fontWeight: 'bold',
+        color: 'white',
+        fontSize: 15,
+      }}>850g</Text>
+    </View>
+    <View style={{
+      width: 140,
+      height: 140,
+      borderRadius: 140 / 2,
+      backgroundColor: '#000000',
+      justifyContent:'center',
+      margin:10,
+      alignSelf:'center'
+    }}>
+      <Text style={{
+        alignSelf: 'center',
+        fontWeight: 'bold',
+        color: 'white',
+        fontSize: 15,
+      }}>25°C</Text>
+    </View>
+    </View>
+    <View style={{justifyContent:'center',flex:1,flexDirection:'row',margin:10}}>
+    <View style={{
+      width: 140,
+      height: 140,
+      borderRadius: 140 / 2,
+      backgroundColor: '#fca311',
+      justifyContent:'center',
+      margin:10,
+      alignSelf:'center'
+    }}>
+      <Text style={{
+        alignSelf: 'center',
+        fontWeight: 'bold',
+        color: 'white',
+        fontSize: 15,
+      }}>11%</Text>
+    </View>
+    <View style={{
+      width: 140,
+      height: 140,
+      borderRadius: 140 / 2,
+      backgroundColor: '#14213d',
+      justifyContent:'center',
+      margin:10,
+      alignSelf:'center'
+    }}>
+      <Text style={{
+        alignSelf: 'center',
+        fontWeight: 'bold',
+        color: 'white',
+        fontSize: 15,
+      }}>3%</Text>
+    </View>
+    </View>
+    </View>
+  /*return(
+    <TouchableHighlight
+    style = {{
+      borderRadius: 45,
+      width: 45,
+      height: 45,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor:'#f00',
+      alignItems: 'center'
+    }}
+    underlayColor = '#ccc'
+    onPress = { () => alert('Yaay!') }
+    >
+    <Text> Mom, look, I am a circle! </Text>
+   </TouchableHighlight>
+  );*/  )
     };
 
 // define your styles
